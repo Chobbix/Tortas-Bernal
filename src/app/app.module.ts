2af9e08802bc1d73843deca8a7d4b0b540879815
Victor (Chobbi) <vic_fco@hotmail.com>
2022-11-03 00:18:43 -0600

    componentes de las views agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { ConfirmOrderComponent } from './confirm-order/confirm-order.component';
+import { RegisterComponent } from './register/register.component';
+import { LocalsComponent } from './locals/locals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent,
+    MenuComponent,
+    OrderComponent,
+    ConfirmOrderComponent,
+    RegisterComponent,
+    LocalsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
